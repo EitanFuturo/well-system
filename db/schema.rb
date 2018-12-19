747699a375564e45597d05c4303beecd5b1590bd
Eitan Abelson <eabelson@infoil.com.ar>
2018-12-19 17:46:56 -0300

    Se agregan autenticacion de usuarios, traducciones y estilos.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181217185023) do
+ActiveRecord::Schema.define(version: 20181219151040) do
 
   create_table "areas", force: :cascade do |t|
     t.string   "name",             limit: 4000
@@ -33,6 +33,19 @@ ActiveRecord::Schema.define(version: 20181217185023) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 4000, default: "", null: false
+    t.string   "encrypted_password",     limit: 4000, default: "", null: false
+    t.string   "reset_password_token",   limit: 4000
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                                       null: false
+    t.datetime "updated_at",                                       null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "wells", force: :cascade do |t|
     t.string   "name",       limit: 4000
